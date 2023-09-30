01169dbf4ba641152660eb07a199a5c883b7299b
Vincent <vincemonerow@gmail.com>
2023-09-30 23:44:22 +0100

    Password Verifer in JS

---
+++ forms.js
@@ -0,0 +1,40 @@
+//Addition 
+function add(a, b){
+    console.log(a+b)
+}
+add(5,4);
+
+function greet(a, b, c){
+    console.log("Hello " + a)
+}
+
+greet("Vince", "Brown", "Tade")
+
+
+function salute(name){
+    console.log("Hi " + name)
+}
+
+salute("Wade");
+
+
+function doubleNumber(num){
+    console.log(num * 2)
+}
+
+doubleNumber(24);
+
+
+//VerifyPassword 
+function verifyPassword(){
+    let firstPassword = document.getElementById('pass').value
+    let secondPassword = document.getElementById('cpass').value
+
+    if (firstPassword === !secondPassword){
+        alert("Grant Access")
+    }else(
+        alert("Deny Access")
+    )
+}
+
+console.log(document)
